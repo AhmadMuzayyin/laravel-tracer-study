af0cc717a471631288bf05216273c7b8b9416392
AhmadMuzayyin <muzayyin654@gmail.com>
2024-05-20 21:42:56 +0700

    Create bar chart by question count

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,42 +16,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        // \App\Models\User::create([
-        //     'name' => 'Admin',
-        //     'last_name' => 'SMKN 1 PROPPO',
-        //     'email' => 'admin@admin.com',
-        //     'email_verified_at' => now(),
-        //     'password' => 'password',
-        //     'remember_token' =>  \Illuminate\Support\Str::random(10),
-        //     'role' => RoleEnum::Admin->value
-        // ]);
-        \App\Models\User::factory(50)->create();
-        // $kuesioner = [
-        //     'Apakah Anda bekerja saat ini?',
-        //     'Apakah Anda berorganisasi ketika masih siswa ?',
-        //     'Jika Tidak berorganisasi, mengapa?',
-        //     'Setelah lulus SMKN 1 Proppo, apakah Anda bersekolah lagi ?',
-        //     'Apa alasan utama Anda bersekolah lagi ?',
-        //     'Kapan Anda mulai mencari pekerjaan ?',
-        //     ' Apakah pendidikan yang Anda dapat di SMKN 1 PROPPO relevan dengan pekerjaan Anda ? ',
-        //     'Dari pengalaman Anda bekerja, apa saran praktis Anda untuk pendidikan di SMKN 1 PROPPO dalam rangka meningkatkan kesesuaian antara pendidikan dengan lapangan pekerjaan ?',
-        //     'Apakah Anda merasa bahwa pendidikan di SMKN 1 PROPPO cukup mempersiapkan Anda untuk bekerja ?',
-        //     'Saat baru lulus, sejauh mana Anda merasa mampu bersaing dng lulusan sekolah lain ?',
-        //     'Sejauh ini, menurut Anda lulusan SMKN 1 PROPPO yang bagaimana yg diperlukan oleh pasar/lapangan kerja ?',
-        //     'Pengetahuan pada disiplin ilmu Anda ? ',
-        //     'Pengetahuan diluar disiplin ilmu Anda ?',
-        //     'Pengetahuan Umum ?',
-        //     'Keterampilan Bahasa Inggris ?',
-        //     'Keterampilan Komputer diluar pelajaran?',
-        //     'Berpikir Kritis ?',
-        //     'Kemampuan Berkomunikasi ?',
-        //     'Analisa masalah ?',
-        // ];
-        // foreach ($kuesioner as $key => $value) {
-        //     \App\Models\Question::create([
-        //         'slug' => \Illuminate\Support\Str::slug($value),
-        //         'name' => $value
-        //     ]);
-        // }
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'last_name' => 'SMKN 1 PROPPO',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'remember_token' =>  \Illuminate\Support\Str::random(10),
+            'role' => RoleEnum::Admin->value
+        ]);
+        $this->call(QuestionSeeder::class);
+        // \App\Models\User::factory(50)->create();
     }
 }
---
+++ database/seeders/QuestionSeeder.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Answer;
+use App\Models\Question;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class QuestionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $questions = [
+            [
+                'question' => 'Apakah Anda bekerja saat ini?',
+                'answers' => [
+                    'Ya',
+                    'Tidak',
+                    'Sedang mencari pekerjaan',
+                    'Sedang melanjutkan pendidikan'
+                ]
+            ],
+            [
+                'question' => 'Berapa lama waktu yang Anda butuhkan untuk mendapatkan pekerjaan pertama setelah lulus?',
+                'answers' => [
+                    'Kurang dari 1 bulan',
+                    '1-3 bulan',
+                    '4-6 bulan',
+                    'Lebih dari 6 bulan'
+                ]
+            ],
+            [
+                'question' => 'Apa jenis pekerjaan Anda saat ini?',
+                'answers' => [
+                    'Pekerjaan tetap',
+                    'Pekerjaan kontrak',
+                    'Pekerjaan paruh waktu',
+                    'Wirausaha'
+                ]
+            ],
+            [
+                'question' => 'Apakah pekerjaan Anda saat ini sesuai dengan bidang studi yang Anda pelajari di SMKN 1 Proppo?',
+                'answers' => [
+                    'Sangat sesuai',
+                    'Cukup sesuai',
+                    'Kurang sesuai',
+                    'Tidak sesuai sama sekali'
+                ]
+            ],
+            [
+                'question' => 'Bagaimana Anda menemukan pekerjaan Anda saat ini?',
+                'answers' => [
+                    'Melalui iklan lowongan kerja',
+                    'Rekomendasi teman/keluarga',
+                    'Job fair',
+                    'Portal pencarian kerja online'
+                ]
+            ],
+            [
+                'question' => 'Apakah Anda merasa pendidikan di SMKN 1 Proppo mempersiapkan Anda dengan baik untuk pekerjaan Anda?',
+                'answers' => [
+                    'Sangat mempersiapkan',
+                    'Cukup mempersiapkan',
+                    'Kurang mempersiapkan',
+                    'Tidak mempersiapkan sama sekali'
+                ]
+            ],
+            [
+                'question' => 'Apa tingkat pendapatan bulanan Anda saat ini?',
+                'answers' => [
+                    'Kurang dari Rp 2.000.000',
+                    'Rp 2.000.000 - Rp 4.000.000',
+                    'Rp 4.000.000 - Rp 6.000.000',
+                    'Lebih dari Rp 6.000.000'
+                ]
+            ],
+            [
+                'question' => 'Apakah Anda pernah mengikuti pelatihan atau kursus tambahan setelah lulus dari SMKN 1 Proppo?',
+                'answers' => [
+                    'Ya, terkait bidang studi',
+                    'Ya, tidak terkait bidang studi',
+                    'Tidak, tetapi berencana',
+                    'Tidak'
+                ]
+            ],
+            [
+                'question' => 'Seberapa sering Anda menggunakan keterampilan yang Anda pelajari di SMKN 1 Proppo dalam pekerjaan Anda?',
+                'answers' => [
+                    'Setiap hari',
+                    'Beberapa kali seminggu',
+                    'Beberapa kali sebulan',
+                    'Jarang atau tidak pernah'
+                ]
+            ],
+            [
+                'question' => 'Bagaimana Anda menilai kualitas fasilitas dan infrastruktur di SMKN 1 Proppo?',
+                'answers' => [
+                    'Sangat baik',
+                    'Baik',
+                    'Cukup',
+                    'Kurang'
+                ]
+            ],
+            [
+                'question' => 'Bagaimana Anda menilai kualitas pengajaran di SMKN 1 Proppo?',
+                'answers' => [
+                    'Sangat baik',
+                    'Baik',
+                    'Cukup',
+                    'Kurang'
+                ]
+            ],
+            [
+                'question' => 'Apakah Anda melanjutkan pendidikan ke jenjang yang lebih tinggi setelah lulus dari SMKN 1 Proppo?',
+                'answers' => [
+                    'Ya',
+                    'Tidak',
+                    'Sedang berencana',
+                    'Tidak, langsung bekerja'
+                ]
+            ],
+            [
+                'question' => 'Jika ya, di mana Anda melanjutkan pendidikan Anda?',
+                'answers' => [
+                    'Universitas',
+                    'Politeknik',
+                    'Akademi',
+                    'Sekolah kejuruan lainnya'
+                ]
+            ],
+            [
+                'question' => 'Bagaimana Anda menilai relevansi kurikulum SMKN 1 Proppo dengan kebutuhan industri saat ini?',
+                'answers' => [
+                    'Sangat relevan',
+                    'Cukup relevan',
+                    'Kurang relevan',
+                    'Tidak relevan sama sekali'
+                ]
+            ],
+            [
+                'question' => 'Apakah Anda merasa jaringan alumni SMKN 1 Proppo membantu dalam pengembangan karir Anda?',
+                'answers' => [
+                    'Sangat membantu',
+                    'Membantu',
+                    'Kurang membantu',
+                    'Tidak membantu sama sekali'
+                ]
+            ],
+            [
+                'question' => 'Apakah Anda pernah menjalani magang selama di SMKN 1 Proppo?',
+                'answers' => [
+                    'Ya, di perusahaan besar',
+                    'Ya, di perusahaan kecil',
+                    'Ya, di lembaga pemerintah',
+                    'Tidak pernah'
+                ]
+            ],
+            [
+                'question' => 'Bagaimana pengalaman magang Anda membantu dalam pekerjaan Anda saat ini?',
+                'answers' => [
+                    'Sangat membantu',
+                    'Cukup membantu',
+                    'Kurang membantu',
+                    'Tidak membantu sama sekali'
+                ]
+            ],
+            [
+                'question' => 'Apakah Anda terlibat dalam organisasi atau kegiatan ekstrakurikuler selama di SMKN 1 Proppo?',
+                'answers' => [
+                    'Ya, aktif di organisasi',
+                    'Ya, aktif di kegiatan ekstrakurikuler',
+                    'Tidak terlalu aktif',
+                    'Tidak terlibat sama sekali'
+                ]
+            ],
+            [
+                'question' => 'Bagaimana pengalaman tersebut mempengaruhi pengembangan keterampilan Anda?',
+                'answers' => [
+                    'Sangat positif',
+                    'Cukup positif',
+                    'Kurang positif',
+                    'Tidak berpengaruh'
+                ]
+            ],
+            [
+                'question' => 'Apakah Anda memiliki saran atau masukan untuk perbaikan SMKN 1 Proppo di masa mendatang?',
+                'answers' => [
+                    'Lebih banyak pelatihan praktis',
+                    'Peningkatan fasilitas',
+                    'Peningkatan kualitas pengajaran',
+                    'Program kerjasama dengan industri'
+                ]
+            ]
+        ];
+        $status = [
+            'pertama', 'kedua', 'ketiga', 'keempat'
+        ];
+        foreach ($questions as $question) {
+            $questionModel = Question::create([
+                'slug' => Str::slug($question['question']),
+                'name' => $question['question'],
+            ]);
+            foreach ($question['answers'] as $key => $answer) {
+                Answer::create([
+                    'question_id' => $questionModel->id,
+                    'jawaban' => $answer,
+                    'status' => $status[$key] // ini bisa disesuaikan atau di-random
+                ]);
+            }
+        }
+    }
+}
